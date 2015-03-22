6e957a7c1f919d5b34b04afa59a2ccb7bffa1a51
guy7145 <guy7145@gmail.com>
2015-03-22 18:31:01 +0200

    gdhj

---
+++ FullInfrastructure/UI/MyUserInterface.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TcpHandlerProject.TcpClientHandler;
+
+namespace UI
+{
+    public class MyUserInterface
+    {
+        MyTcpClient cl;
+        bool _running;
+
+        public MyUserInterface(MyTcpClient cl)
+        {
+            this.cl = cl;
+            _running = false;
+        }
+        public void Run()
+        {
+            Console.Clear();
+            _running = true;
+            string s;
+
+            while (_running)
+            {
+                Console.WriteLine(cl.StreamOut("printMenu"));
+                Console.Write("input >> ");
+                s = Console.ReadLine();
+                Console.WriteLine(cl.StreamOut(s));
+                Console.WriteLine();
+            }
+
+
+        }
+        public void RequestStop()
+        {
+            this._running = false;
+        }
+    }
+}
