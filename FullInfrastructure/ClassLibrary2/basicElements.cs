89377328095f2471261212fe7ae2d60c2e91be1c
MiaoHatola <Jalon0@gmail.com>
2015-03-22 18:02:21 +0200

    Full Infrastructure. From now on we keep it organized! :)

---
+++ FullInfrastructure/ClassLibrary2/basicElements.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    class department
+    {
+        private int ID;
+        private string name;
+        public department(int id, string name)
+        {
+            this.name = name;
+            this.ID = id;
+        }
+        public string getName()
+        {
+            return this.name;
+        }
+        public int getId()
+        {
+            return this.ID;
+        }
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+        public void setId(int id)
+        {
+            this.ID = id;
+        }
+    }
+    class employee
+    {
+
+    }
+    class transaction
+    {
+
+    }
+    class product
+    {
+
+    }
+    class user
+    {
+
+    }
+    class clubMember
+    {
+
+    }
+    class person
+    {
+
+    }
+}
+
---
+++ FullInfrastructure/ClassLibrary2/serializeHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Database
+{
+    class serializeHandler
+    {
+        private byte[] Key = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16 };
+        private byte[] IV = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16 };
+        private Stream myFileStream;
+        private RijndaelManaged RMCrypto = new RijndaelManaged();
+        private BinaryFormatter ser = new BinaryFormatter();
+        private CryptoStream CryptStream;
+
+
+        public serializeHandler()
+        {
+            RMCrypto.Padding = PaddingMode.Zeros;           
+        }
+        public void serialize(string filename,object toSave)
+        {
+            myFileStream = File.Create(filename);
+            CryptStream = new CryptoStream(myFileStream, RMCrypto.CreateEncryptor(Key,IV), CryptoStreamMode.Write);
+            ser.Serialize(CryptStream, toSave);
+            CryptStream.FlushFinalBlock();
+            CryptStream.Close();
+            myFileStream.Close();
+        }
+        public Object deserialize(string filename)
+        {
+            if (File.Exists(filename))
+            {
+                myFileStream = File.OpenRead(filename);
+                CryptStream = new CryptoStream(myFileStream, RMCrypto.CreateDecryptor(Key, IV), CryptoStreamMode.Read);
+                Object toReturn = (ser.Deserialize(CryptStream) as Object);
+                myFileStream.Close();
+                CryptStream.Close();
+                return toReturn;
+            }
+            return null;
+        }
+    }
+}
