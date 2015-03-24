7bf2ead94108b0f1133e17a8d2b183ad5d6a300c
MiaoHatola <Jalon0@gmail.com>
2015-03-24 22:01:29 +0200

    Final change before merge

---
+++ FullInfrastructure/Database/transactionDB.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    class transactionDB
+    {
+        List<transaction> transactions;
+        readonly string FILENAME="transactions.bin";
+        public transactionDB(bool isNew)
+        {
+            //Creates a new department database
+            if(isNew)
+                transactions = new List<transaction>();
+        }
+
+        public bool load()
+        {
+            serializeHandler ser=new serializeHandler();
+            return(ser.deserialize(FILENAME)!=null);
+        }
+        public void save()
+        {
+            serializeHandler ser = new serializeHandler();
+            ser.serialize(FILENAME, transactions);
+        }
+    }
+}
